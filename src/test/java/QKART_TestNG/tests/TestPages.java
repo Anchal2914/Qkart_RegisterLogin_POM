007d83b8c83d26e1c9a2bdf23fe4487ef9effd5e
CrioUser <crio-user@criodo.com>
2023-06-18 11:18:16 +0530

    Changed navigateToRegisterPage assertion

--- src/test/java/QKART_TestNG/tests/TestPages.java
+++ src/test/java/QKART_TestNG/tests/TestPages.java
@@ -8,52 +8,58 @@ import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
+import org.testng.asserts.Assertion;
 
 public class TestPages {
 
-    static RemoteWebDriver driver;
+	static RemoteWebDriver driver;
 
-    // Method to help us log our Unit Tests
+	// Method to help us log our Unit Tests
 	public static void logStatus(String type, String message, String status) {
-    	System.out.println(String.format("%s |  %s  |  %s | %s", String.valueOf(java.time.LocalDateTime.now()), type,
-            	message, status));
+		System.out.println(String.format("%s |  %s  |  %s | %s",
+				String.valueOf(java.time.LocalDateTime.now()), type, message, status));
 	}
-    // Iinitialize webdriver for our Unit Tests
+
+	// Iinitialize webdriver for our Unit Tests
 	@BeforeClass(alwaysRun = true, enabled = true)
-	public static void createDriver() throws MalformedURLException{
-    	logStatus("driver","Initializing driver","Started");
-    	final DesiredCapabilities capabilities = new DesiredCapabilities();
-    	capabilities.setBrowserName(BrowserType.CHROME);
-    	driver = new RemoteWebDriver(new URL("http://localhost:8082/wd/hub"), capabilities);
-    	logStatus("driver","Initializing driver","Success");   	 
+	public static void createDriver() throws MalformedURLException {
+		logStatus("driver", "Initializing driver", "Started");
+		final DesiredCapabilities capabilities = new DesiredCapabilities();
+		capabilities.setBrowserName(BrowserType.CHROME);
+		driver = new RemoteWebDriver(new URL("http://localhost:8082/wd/hub"), capabilities);
+		logStatus("driver", "Initializing driver", "Success");
 	}
 
-    // Quit webdriver after Unit Tests
-    @AfterClass(enabled = true)
-	public static void quitDriver() throws MalformedURLException{
-    	driver.close();
-        driver.quit();
-    	logStatus("driver","Quitting driver","Success");   	 
-	}
 
-    // TODO: Use the following to test --navigateToRegisterPage()-- method 
 
-    @Test(description = "Verify functionality of - navigate to register page", enabled = true)
+	// TODO: Use the following to test --navigateToRegisterPage()-- method
+
+	@Test(description = "Verify functionality of - navigate to register page", enabled = true)
 	public static void testRegister_navigateToRegisterPage() {
-    	logStatus("Page test", "navigation to register page", "started");
-    	try {
-        	Register register = new Register(driver);
-        	register.navigateToRegisterPage();
-        	logStatus("Page test", "navigation to register page", "success");
-    	} catch (Exception e) {
-        	logStatus("Page test", "navigation to register page", "failed");
-        	e.printStackTrace();
-    	}
+		Assertion assertion = new Assertion();
+		logStatus("Page test", "navigation to register page", "started");
+		try {
+			Register register = new Register(driver);
+			register.navigateToRegisterPage();
+			assertion.assertTrue(driver.getCurrentUrl()=="https://crio-qkart-frontend-qa.vercel.app/register");
+			logStatus("Page test", "navigation to register page", "success");
+		} catch (Exception e) {
+			logStatus("Page test", "navigation to register page", "failed");
+			e.printStackTrace();
+		}
 	}
 
 
-    // TODO: Implement Rest of Unit Tests Here-
+	// TODO: Implement Rest of Unit Tests Here-
 
-    
+
+
+	// Quit webdriver after Unit Tests
+	@AfterClass(enabled = true)
+	public static void quitDriver() throws MalformedURLException {
+		driver.close();
+		driver.quit();
+		logStatus("driver", "Quitting driver", "Success");
+	}
 
 }
